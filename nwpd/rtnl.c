6b760ebc60b38a4fc93a5f14807f5b470a7878f3
Vibhav Pant <vibhavp@gmail.com>
2018-06-29 00:39:08 +0530

    Add RTNETLINK parsing functions + dump functions.

--- nwpd/rtnl.c
+++ nwpd/rtnl.c
@@ -2,40 +2,53 @@
 #include <linux/rtnetlink.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
+#include "rtnl.h"
 
-typedef void (*rtnl_filter_t)(struct nlmsghdr *n, void *);
-
-int rtnl_talk(const struct mnl_socket *nl_socket, struct nlmsghdr *n,
-              const struct nlmsghdr *reply)
+int rtnl_talk(const struct mnl_socket *nl_socket, struct nlmsghdr *n)
 {
-        if (reply == NULL)
-                n->nlmsg_flags |= NLM_F_ACK;
+        uint32_t seq = time(NULL);
+        n->nlmsg_seq = seq;
+        n->nlmsg_flags |= NLM_F_ACK;
 
         if (mnl_socket_sendto(nl_socket, n, n->nlmsg_len) < 0) {
                 perror("mnl_socket_sendto");
                 return -1;
         }
+
         char recv_buf[MNL_SOCKET_BUFFER_SIZE];
         struct nlmsghdr *nlh = mnl_nlmsg_put_header(recv_buf);
+        int msglen = sizeof(recv_buf);
 
         if (mnl_socket_recvfrom(nl_socket, recv_buf, sizeof(recv_buf)) == -1) {
                 perror("mnl_socket_recvfrom");
                 return -1;
         }
 
+        if (!mnl_nlmsg_ok(nlh, msglen)) {
+                fprintf(stderr, "rtnl_talk: Received malformed/truncated message");
+                return -1;
+        }
+
         while(1) {
-                if (!mnl_nlmsg_ok(nlh, sizeof(recv_buf))) {
-                        fprintf(stderr, "rtnl_talk: Received malformed/truncated message");
-                        return -1;   
+                if (!mnl_nlmsg_ok(nlh, msglen)) {
+                        return 0;
+                }
+
+                if (nlh->nlmsg_seq != seq) {
+                        printf("getting next\n");
+                        nlh = mnl_nlmsg_next(nlh, &msglen);
+                        continue;
                 }
                 if (nlh->nlmsg_type == NLMSG_ERROR) {
-                        struct nlmsgerr *err = NLMSG_DATA(nlh);
+                        struct nlmsgerr *err = mnl_nlmsg_get_payload(nlh);
                         errno = -err->error;
-                        if (errno == 0) 
+                        if (errno == 0)
                                 return 0;
                         perror("RTNETLINK");
                         return -1;
                 }
+                nlh = mnl_nlmsg_next(nlh, &msglen);
         }
 }
 
@@ -46,6 +59,7 @@ int rtnl_send_wilddump_request(const struct mnl_socket *nl_socket,
         char buf[MNL_SOCKET_BUFFER_SIZE];
         struct nlmsghdr *nlh = mnl_nlmsg_put_header(buf);
         int rtrn;
+        uint32_t seq = time(NULL);
 
         {
                 struct rtgenmsg *g = mnl_nlmsg_put_extra_header(nlh,
@@ -54,26 +68,48 @@ int rtnl_send_wilddump_request(const struct mnl_socket *nl_socket,
                 g->rtgen_family = family;
                 nlh->nlmsg_type = type;
                 nlh->nlmsg_flags = NLM_F_ROOT | NLM_F_MATCH | NLM_F_REQUEST;
-                nlh->nlmsg_seq = time(NULL);
+                nlh->nlmsg_seq = seq;
 
-                if ((rtrn = mnl_socket_sendto(nl_socket, nlh, nlh->nlmsg_len)))
+                if ((rtrn = mnl_socket_sendto(nl_socket, nlh, nlh->nlmsg_len)) == -1)
                         return rtrn;
         }
-        
+
         nlh = mnl_nlmsg_put_header(buf);
-        if ((rtrn = mnl_socket_recvfrom(nl_socket, buf, sizeof(buf))))
+        if ((rtrn = mnl_socket_recvfrom(nl_socket, buf, sizeof(buf))) == -1)
                 return rtrn;
-        while (1) {
-                if (!mnl_nlmsg_ok(nlh, sizeof(buf))) {
-                        fprintf(stderr, "rtnl_talk: Received malformed/truncated message");
-                        return -1;
-                }
+
+        int msglen = sizeof(buf);
+        while (mnl_nlmsg_ok(nlh, msglen)) {
                 if (nlh->nlmsg_type == NLMSG_ERROR) {
-                        struct nlmsgerr *err = NLMSG_DATA(nlh);
+                        struct nlmsgerr *err = mnl_nlmsg_get_payload(nlh);
                         errno = -err->error;
                         perror("RTNETLINK");
                         return -1;
                 }
                 callback(nlh, arg);
+                nlh = mnl_nlmsg_next(nlh, &msglen);
         }
+
+        return 0;
+}
+
+int parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, int len)
+{
+	memset(tb, 0, sizeof(struct rtattr *) * (max + 1));
+	while (RTA_OK(rta, len)) {
+		if ((rta->rta_type <= max) && (!tb[rta->rta_type]))
+			tb[rta->rta_type] = rta;
+		rta = RTA_NEXT(rta,len);
+	}
+	if (len)
+		fprintf(stderr, "!!!Deficit %d, rta_len=%d\n", len, rta->rta_len);
+	return 0;
+}
+
+int rtnl_get_table(struct rtmsg *r, struct rtattr **tb)
+{
+	__u32 table = r->rtm_table;
+	if (tb[RTA_TABLE])
+		table = *(__u32*)RTA_DATA(tb[RTA_TABLE]);
+	return table;
 }
