aae438e64a6bb365cc5a590b917ea432c51ca5c5
Vibhav Pant <vibhavp@gmail.com>
2018-06-29 00:39:47 +0530

    Remove announce_validate, replace with read_announce.

--- nwpd/nwp.c
+++ nwpd/nwp.c
@@ -3,21 +3,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-bool announce_validate(struct nwp_announce *packet, int msglen)
+bool read_announce(char *buf, struct nwp_announce *packet, int msglen)
 {
         size_t elem_len = sizeof(struct nwp_common_hdr)
                 + sizeof(uint8_t)
                 + sizeof(uint8_t);
-        if (msglen < elem_len)
+        size_t data_len = packet->haddr_len + XIA_XID_MAX * packet->hid_count;
+        if (elem_len + data_len > msglen)
                 return false;
 
-        packet->haddr = (uint8_t *)(packet +
-                                      offsetof(struct nwp_announce, haddr));
-        packet->addr_begin = (uint8_t **)(packet->haddr
-                                            + packet->haddr_len);
-        return ((elem_len + XIA_XID_MAX * packet->hid_count + packet->haddr_len)
-                 <= msglen);
+        memcpy(packet, buf, elem_len);
+
+        buf = buf + elem_len;
+        packet->haddr = malloc(packet->haddr_len);
+        memcpy(packet->haddr, buf, packet->haddr_len);
+
+        buf = buf + packet->haddr_len;
+        packet->addr_begin = malloc(XIA_XID_MAX * packet->hid_count);
+        memcpy(packet->addr_begin, buf, XIA_XID_MAX * packet->hid_count);
+        return true;
+}
+
+void announce_set_ha(struct nwp_announce *packet, uint8_t *ha)
+{
+        assert(packet->haddr_len != 0);
+        memmove(packet->haddr, ha, packet->haddr_len);
+} 
+
+void announce_add_xid(struct nwp_announce *packet, uint8_t *xid)
+{
+        memmove(packet->addr_begin + ((packet->hid_count++) * XIA_XID_MAX),
+                xid, XIA_XID_MAX);
+}
+
+int announce_size(struct nwp_announce *packet)
+{
+        return sizeof(struct nwp_common_hdr) + 2*sizeof(uint8_t)
+                + packet->haddr_len + packet->hid_count * XIA_XID_MAX;
 }
 
 bool neigh_list_validate(struct nwp_neigh_list *packet, int msglen)
@@ -25,6 +50,7 @@ bool neigh_list_validate(struct nwp_neigh_list *packet, int msglen)
         size_t size = sizeof(struct nwp_common_hdr)
                 + sizeof(uint8_t)
                 + sizeof(uint8_t);
+        packet->addr_begin = (uint8_t *)packet + offsetof(struct nwp_neigh_list, addr_begin);
         int i;
         uint8_t *addr = packet->addr_begin + 1;
 
