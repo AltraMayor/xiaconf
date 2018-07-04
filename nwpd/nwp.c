05c785debc36da74e6827056008850487d04e13a
Vibhav Pant <vibhavp@gmail.com>
2018-07-04 15:47:12 +0530

    read_announce: Copy from buffer correctly

--- nwpd/nwp.c
+++ nwpd/nwp.c
@@ -10,20 +10,25 @@ bool read_announce(char *buf, struct nwp_announce *packet, int msglen)
 {
         size_t elem_len = sizeof(struct nwp_common_hdr)
                 + sizeof(uint8_t)
-                + sizeof(uint8_t);
-        size_t data_len = packet->haddr_len + XIA_XID_MAX * packet->hid_count;
+                + sizeof(uint8_t), data_len;
+
+        if (elem_len > msglen)
+                return false;
+        memcpy(packet, buf, elem_len);
+        buf += elem_len;
+
+        data_len = packet->haddr_len + XIA_XID_MAX * packet->hid_count;
+
         if (elem_len + data_len > msglen)
                 return false;
 
-        memcpy(packet, buf, elem_len);
-
-        buf = buf + elem_len;
         packet->haddr = malloc(packet->haddr_len);
         memcpy(packet->haddr, buf, packet->haddr_len);
+        buf += packet->haddr_len;
 
-        buf = buf + packet->haddr_len;
         packet->addr_begin = malloc(XIA_XID_MAX * packet->hid_count);
         memcpy(packet->addr_begin, buf, XIA_XID_MAX * packet->hid_count);
+
         return true;
 }
 
