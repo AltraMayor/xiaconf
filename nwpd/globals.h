ad9a6192c4e5bfe4ca4d4306f3527995d740ecc2
Vibhav Pant <vibhavp@gmail.com>
2018-08-01 22:40:53 +0530

    Add globals.h

---
+++ nwpd/globals.h
@@ -0,0 +1,28 @@
+#ifndef _GLOBALS_H
+#define _GLOBALS_H
+#include <libmnl/libmnl.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+
+extern struct mnl_socket *xia_nl_socket;
+extern struct sockaddr_ll broadcast_addr;
+extern int eth_socket;
+extern uint8_t if_hwaddr[ETH_ALEN];
+
+struct config {
+        /* The time period (in seconds) for try announcing to the network */
+        int try_announce_period;
+        /* The time period (in seconds) for attemping to ping a neighbor. */
+        int monitor_ping_period;
+        int monitor_ack_timeout;
+        /* The timeout period for an investigative ack before nwpd removes
+         a neighbor. */
+        int monitor_investigative_ack_timeout;
+        /* The number of neighbours nwpd sends an investigative ping request to
+           on a timeout. */
+        int monitor_investigative_neigh_count;
+};
+
+extern struct config nwpd_config;
+
+#endif /* _GLOBALS_H */
