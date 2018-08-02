b826ebf055f3607436d8f9ab32e974ad99ef4700
Vibhav Pant <vibhavp@gmail.com>
2018-08-02 18:27:42 +0530

    Fix config initialization

--- nwpd/config.c
+++ nwpd/config.c
@@ -7,6 +7,7 @@
 #include "log.h"
 
 struct config nwpd_config = {
+        .log_level = LOG_LEVEL_INFO,
         .try_announce_period = 2,
         .monitor_ping_period = 5,
         .monitor_ack_timeout = 5,
@@ -14,7 +15,23 @@ struct config nwpd_config = {
         .monitor_investigative_neigh_count = 10,
 };
 
-bool check_num(char *str)
+static char const *const usage_message =
+        "usage: dhcpcd \t -i interface [-hlapkvn]\n"
+        "\t\t[-h, --help]\n"
+        "\t\t[-i, --interface iface]\n"
+        "\t\t[-l, --log-level level]\n"
+        "\t\t[-a, --try-announce seconds]\n"
+        "\t\t[-p, --ping-period seconds]\n"
+        "\t\t[-k, --ack-timeout seconds]\n"
+        "\t\t[-v, --ivst-ack-timeout seconds]\n"
+        "\t\t[-n, --ivst-neigh number]\n";
+
+static void print_help()
+{
+        fprintf(stderr, usage_message);
+}
+
+static bool check_num(char *str)
 {
         int i;
         for (i = 0; i < strlen(str); i++)
@@ -30,80 +47,94 @@ void init_config(int argc, char **argv)
         while (1) {
                 static struct option long_options[] =
                         {
-                                {"log-level",        optional_argument, NULL, 'l'},
-                                {"try-announce",     optional_argument, NULL, 'a'},
-                                {"ping-period",      optional_argument, NULL, 'p'},
-                                {"ack-timeout",      optional_argument, NULL, 'k'},
-                                {"ivst-ack-timeout", optional_argument, NULL, 'v'},
-                                {"ivst-neigh",       optional_argument, NULL, 'n'},
-
-                                {"interface",        required_argument, NULL, 'i'},
-                                {0,0,NULL,0}
+                                {"help",             no_argument,       NULL,             'h'},
+                                {"log-level",        required_argument, NULL,             'l'},
+                                {"try-announce",     required_argument, NULL,             'a'},
+                                {"ping-period",      required_argument, NULL,             'p'},
+                                {"ack-timeout",      required_argument, NULL,             'k'},
+                                {"ivst-ack-timeout", required_argument, NULL,             'v'},
+                                {"ivst-neigh",       required_argument, NULL,             'n'},
+                                {"interface",        required_argument, NULL,             'i'},
+                                {NULL,0,NULL,0}
                         };
                 int index;
 
-                c = getopt_long(argc, argv, "lapkvni:", long_options, &index);
+                c = getopt_long(argc, argv, "hl:a:p:k:v:n:i:", long_options, &index);
                 if (c == -1)
                         break;
-                switch (c) {
+                switch ((char)c) {
+                case 'h':
+                        print_help();
+                        exit(0);
                 case 'i':
                         nwpd_config.interface = strdup(optarg);
                         break;
                 case 'l':
-                        if (strcmp(optarg, "debug"))
+                        if (strcmp(optarg, "debug") == 0)
                                 nwpd_config.log_level = LOG_LEVEL_DEBUG;
-                        else if (strcmp(optarg, "info"))
+                        else if (strcmp(optarg, "info") == 0)
                                 nwpd_config.log_level = LOG_LEVEL_INFO;
-                        else if (strcmp(optarg, "warning"))
+                        else if (strcmp(optarg, "warning") == 0)
                                 nwpd_config.log_level = LOG_LEVEL_WARNING;
-                        else if (strcmp(optarg, "error"))
+                        else if (strcmp(optarg, "error") == 0)
                                 nwpd_config.log_level = LOG_LEVEL_ERROR;
-                        else if (strcmp(optarg, "fatal"))
+                        else if (strcmp(optarg, "fatal") == 0)
                                 nwpd_config.log_level = LOG_LEVEL_FATAL;
                         else {
-                                fprintf(stderr, "invalid log level value\n");
+                                fprintf(stderr, "nwpd: invalid log level value\n");
+                                print_help();
                                 exit(1);
                         }
                         break;
                 case 'a':
                         if (check_num(optarg)) {
-                                fprintf(stderr, "invalid announce period value\n");
+                                fprintf(stderr, "nwpd: invalid announce period value\n");
+                                print_help();
                                 exit(1);
                         }
                         nwpd_config.try_announce_period = atoi(optarg);
                         break;
                 case 'p':
                         if (check_num(optarg)) {
-                                fprintf(stderr, "invalid ping period value\n");
+                                fprintf(stderr, "nwpd: invalid ping period value\n");
+                                print_help();
                                 exit(1);
                         }
                         nwpd_config.monitor_ping_period = atoi(optarg);
                         break;
                 case 'k':
                         if (check_num(optarg)) {
-                                fprintf(stderr, "invalid ack timeout value\n");
+                                fprintf(stderr, "nwpd: invalid ack timeout value\n");
+                                print_help();
                                 exit(1);
                         }
                         nwpd_config.monitor_ack_timeout = atoi(optarg);
                         break;
                 case 'v':
                         if (check_num(optarg)) {
-                                fprintf(stderr, "invalid investigative ack timeout value\n");
+                                fprintf(stderr, "nwpd: invalid investigative ack timeout value\n");
+                                print_help();
                                 exit(1);
                         }
                         nwpd_config.monitor_investigative_ack_timeout = atoi(optarg);
                         break;
                 case 'n':
                         if (check_num(optarg)) {
-                                fprintf(stderr, "invalid investigative neighbor count value\n");
+                                fprintf(stderr, "nwpd: invalid investigative neighbor count value\n");
+                                print_help();
                                 exit(1);
                         }
                         nwpd_config.monitor_investigative_neigh_count = atoi(optarg);
                         break;
                 default:
-                        fprintf(stderr, "missing/invalid options\n");
+                        fprintf(stderr, "nwpd:missing/invalid options\n");
+                        print_help();
                         exit(1);
                         break;
                 }
+        }
+
+        if (nwpd_config.interface == NULL) {
+                fprintf(stderr, "nwpd: interface not mentioned\n");
         }
 }
